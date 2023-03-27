005237e68271122ebc918c87694e51ced41f1bc3
utkarsh_raj <rajutkarsh2505@gmail.com>
2023-03-28 03:57:45 +0530

    made folder structure and configured eslint

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Home, Add, Gig, Gigs, Login, Message, Messages, MyGigs, Orders, Register Error } from "./pages/index";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </Router>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ client/src/pages/Home/Home.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Home.scss";
+
+const Home = () => {
+  return <div>Homepage</div>;
+};
+
+export default Home;
